c001e01be1f2dbb7c1e6c2ed8431181437c7532d
maksymsiruk <maksymsiruk@gmail.com>
2013-02-08 01:17:15 +0200

    first commit

---
+++ SaveViewController.h
@@ -0,0 +1,14 @@
+//
+//  SaveViewController.h
+//  Smooth Drawing
+//
+//  Created by Maksym Siruk on 1/29/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SaveViewController : UIViewController
+- (IBAction)savePDFromLayer:(UIButton *)sender;
+
+@end
---
+++ SaveViewController.m
@@ -0,0 +1,40 @@
+//
+//  SaveViewController.m
+//  Smooth Drawing
+//
+//  Created by Maksym Siruk on 1/29/13.
+//
+//
+
+#import "SaveViewController.h"
+
+@interface SaveViewController ()
+
+@end
+
+@implementation SaveViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)savePDFromLayer:(UIButton *)sender {
+}
+@end
